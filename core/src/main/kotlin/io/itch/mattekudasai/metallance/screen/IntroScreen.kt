a01cd01c7aa288d625a6f849b2e85340d52746d5
matte <matteplaysmahjong@gmail.com>
2024-04-16 07:08:43 +0100

    Day 13: intro transitions

--- core/src/main/kotlin/io/itch/mattekudasai/metallance/screen/IntroScreen.kt
+++ core/src/main/kotlin/io/itch/mattekudasai/metallance/screen/IntroScreen.kt
@@ -2,6 +2,7 @@ package io.itch.mattekudasai.metallance.screen
 
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.audio.Music
+import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
@@ -12,12 +13,15 @@ import io.itch.mattekudasai.metallance.util.disposing.Disposing
 import io.itch.mattekudasai.metallance.util.disposing.Self
 import io.itch.mattekudasai.metallance.util.drawing.DelayedTextDrawer
 import io.itch.mattekudasai.metallance.util.drawing.MonoSpaceTextDrawer
+import io.itch.mattekudasai.metallance.util.drawing.withTransparency
 import io.itch.mattekudasai.metallance.util.files.overridable
 import io.itch.mattekudasai.metallance.util.pixel.intFloat
 import ktx.app.KtxInputAdapter
 import ktx.app.KtxScreen
 import ktx.app.clearScreen
 import ktx.graphics.use
+import kotlin.math.max
+import kotlin.math.min
 
 class IntroScreen(val finish: () -> Unit) : KtxScreen, KtxInputAdapter, Disposing by Self() {
 
@@ -39,11 +43,14 @@ class IntroScreen(val finish: () -> Unit) : KtxScreen, KtxInputAdapter, Disposin
     private val delayedTextDrawer = DelayedTextDrawer(textDrawer, characterTime)
     private val textures = images.map { Texture(it.overridable).autoDisposing() }
     private val music: Music by remember { Gdx.audio.newMusic("music/intro.ogg".overridable) }
+    private var fadeInFor: Float = 0f
+    private var fadeOutFor: Float = 0f
 
     private val sequence = listOf(
         0.5f to {}, // wait
         0f to {
             if (textIndex < textArray.size) {
+                fadeInFor = FADE_TIME
                 val text = textArray[textIndex++]
                 val textDelay = text.sumOf { it.length } * characterTime + 2f
                 delayedTextDrawer.startDrawing(text, viewport.worldWidth / 2f, 0f)
@@ -51,7 +58,9 @@ class IntroScreen(val finish: () -> Unit) : KtxScreen, KtxInputAdapter, Disposin
                 currentWaitTime += textDelay
             }
         },
-        1f to { }, // wait
+        1f to {
+            fadeOutFor = FADE_TIME
+          }, // wait
         0f to {
             music.stop()
             finish()
@@ -60,6 +69,7 @@ class IntroScreen(val finish: () -> Unit) : KtxScreen, KtxInputAdapter, Disposin
     private var textIndex = 0
     private var actionIndex = 0
     private var currentWaitTime = 0f
+    private val transparentColor = Color.WHITE.cpy()
 
     override fun render(delta: Float) {
         clearScreen(red = 0f, green = 0f, blue = 0f)
@@ -71,7 +81,21 @@ class IntroScreen(val finish: () -> Unit) : KtxScreen, KtxInputAdapter, Disposin
             music.pause()
         } else if (!music.isPlaying) {
             music.play()
+            music.volume = musicVolume
             music.isLooping = true
+        }
+
+        if (fadeInFor > 0f) {
+            fadeInFor = max(0f, fadeInFor - delta)
+            transparentColor.a = min(1f, 1f - fadeInFor / FADE_TIME)
+        }
+        if (fadeOutFor > 0f) {
+            fadeOutFor = max(0f, fadeOutFor - delta)
+            transparentColor.r = 1f
+            transparentColor.g = 1f
+            transparentColor.b = 1f
+            transparentColor.a = max(0f, fadeOutFor / FADE_TIME)
+            music.volume = musicVolume * transparentColor.a
         }
 
         currentWaitTime -= delta
@@ -86,11 +110,29 @@ class IntroScreen(val finish: () -> Unit) : KtxScreen, KtxInputAdapter, Disposin
         }
 
         viewport.apply(true)
-        batch.use(camera) {
-            if (textIndex-1 >= 0) {
-                it.draw(textures[textIndex-1], 0f.intFloat, 32f.intFloat)
+        withTransparency {
+            batch.use(camera) {
+                if (textIndex - 1 >= 0) {
+                    if (actionIndex == 1) {
+                        if (transparentColor.a < 1f && textIndex - 2 >= 0) {
+                            it.color = Color.WHITE
+                            it.draw(textures[textIndex - 2], 0f.intFloat, 32f.intFloat)
+                            transparentColor.r = transparentColor.a
+                            transparentColor.g = transparentColor.a
+                            transparentColor.b = transparentColor.a
+                        } else {
+                            transparentColor.r = 1f
+                            transparentColor.g = 1f
+                            transparentColor.b = 1f
+                        }
+                    }
+                    it.color = transparentColor
+                    it.draw(textures[textIndex - 1], 0f.intFloat, 32f.intFloat)
+
+                }
+                it.color = if (actionIndex > 1) transparentColor else Color.WHITE
+                delayedTextDrawer.updateAndDraw(delta, batch)
             }
-            delayedTextDrawer.updateAndDraw(delta, batch)
         }
         super.render(delta)
     }
@@ -112,26 +154,17 @@ class IntroScreen(val finish: () -> Unit) : KtxScreen, KtxInputAdapter, Disposin
     }
 
     companion object {
+        private const val musicVolume = 1f
         private const val characterTime = 0.04f
         private val lines = """
         20XX
 
         Earth is invaded by alien race
-        That conquered the universe
-
-        For many years people were enslaved
-
-        In total secrecy
-        Deep underground
-
-        They put everything they had
-
-        Into creating the ultimate weapon
-        the last hope of humanity
-        """.trimIndent().uppercase()
+        That conquered the universe""".trimIndent().uppercase()
 
         private val textArray = lines.split("\n\n").map { it.split("\n") }
         private val images = textArray.indices.map { "texture/intro/intro$it.png" }
+        private const val FADE_TIME = 0.5f
     }
 
 }
